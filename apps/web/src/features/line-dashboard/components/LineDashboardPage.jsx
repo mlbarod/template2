679106cf8fe396c10e19bd55d05545549f9b4e30
jp <pjw7536@gmail.com>
2026-03-07 14:10:34 +0900

    fix

--- apps/web/src/features/line-dashboard/components/LineDashboardPage.jsx
+++ apps/web/src/features/line-dashboard/components/LineDashboardPage.jsx
@@ -1,16 +1,13 @@
 // src/features/line-dashboard/components/LineDashboardPage.jsx
-import { LineDashboardProvider } from "./LineDashboardProvider"
 import { DataTable } from "./DataTable"
 
 // 라인별 컨텍스트를 구성하고 데이터 테이블을 감싸는 최상위 페이지 컴포넌트입니다.
 export function LineDashboardPage({ lineId }) {
   return (
-    <LineDashboardProvider lineId={lineId}>
-      <div className="flex h-full flex-col gap-4">
-        <div className="flex-1 min-h-0">
-          <DataTable lineId={lineId} />
-        </div>
+    <div className="flex h-full flex-col gap-4">
+      <div className="flex-1 min-h-0">
+        <DataTable lineId={lineId} />
       </div>
-    </LineDashboardProvider>
+    </div>
   )
 }
